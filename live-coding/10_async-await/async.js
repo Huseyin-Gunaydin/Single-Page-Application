a3aeb9e7a639d009df353e9cb9efd949d0693ccf
Huseyin-Gunaydin <gunaydinhsueyin1453@gmail.com>
2022-10-11 12:29:14 +0200

    async

--- live-coding/10_async-await/async.js
+++ live-coding/10_async-await/async.js
@@ -1,39 +1,66 @@
-/* function isZero(number){
-    return new Promise((resolve, reject)=>{
-        if(number==0){
-            resolve("is Zero")
-        }else{
-            reject("not Zero")
+function isZero(number) {
+    return new Promise((resolve, reject) => {
+        if(number == 0) {
+            setTimeout(() => {
+                resolve("Yolo!!!")
+            }, 1000)
+        } else {
+            reject("Not Zero")
         }
     })
 }
-isZero(1000)
-.then((resolvedValue)=>{
-    console.log(resolvedValue)
-})
-.catch((rejectedValue)=>{
-    console.log(rejectedValue)
-})
 
- */
+/* isZero(0)
+    .then((resolvedValue) => {
+        console.log(resolvedValue)
+    })
+    .catch((rejectedValue) => {
+        console.log(rejectedValue)
+    }) */
 
+async function handleNumbers () {
+    const value = await isZero(0)
+    console.log(value)
+}
+handleNumbers()
 
-async function isZeroAdvanced(number){
-    if(number == 0){
-        return setTimeout(()=>"Is Zero", 1000)
-    }else{
+function playFootball() {
+    return new Promise((resolve, reject) => {
+        //Math.random() gibt eine Zahl zwischen 0 und 1 zurück. Bspw: 0.13456 oder 0.87321...
+        const number= Math.random()
+        if (number > 0.8) {
+            resolve("Gewonnen")
+        } else {
+            reject("Verloren")
+        }
+    })
+}
+
+async function matchDay() {
+    try {
+        const outcome = await playFootball()
+        console.log(outcome)
+    } catch (error) {
+        console.log(error)
+    }
+}
+matchDay()
+
+/** 
+ * Wenn eine Funktion als async deklariert wird, gibt sie immer ein Promise zurück.
+ * Wir haben dann Zugriff auf die then() und catch() Funktionen
+ * */
+async function isZeroAdvanced (number) {
+    if(number == 0) {
+         return "Is Zero"
+    }
+    else {
         return "Not Zero"
     }
 }
 
-const returnIsZeroAdvanced = isZeroAdvanced(0)
-console.log(returnIsZeroAdvanced)
+/* isZeroAdvanced(0)
+    .then(resolvedValue => {
+        console.log(resolvedValue)
+    }) */
 
-
-isZeroAdvanced(0)
-.then(resolvedValue =>{
-    console.log(resolvedValue)
-})
-.catch((rejectedValue)=>{
-    console.log(rejectedValue)
-}) 
--- live-coding/10_async-await/index.js
+++ live-coding/10_async-await/index.js
@@ -1,98 +1,89 @@
-/* const inventory={
-    laptop:32,
-    speaker:65,
-    monitor:12
-}
-function checkInventory(product){
-    return new Promise((resolve, reject)=>{
-        if(inventory[product]>0){
-            resolve("Produkt vorhanden")
-        }else{
-            reject("Produkt ausverkauft")
-        }
-    })
-}
-
-
-function order (product){
-   checkInventory(product).then((resolved)=>{
-    console.log(resolved)
-   })
-   .catch((rejected)=>{
-    console.log(rejected)
-   })
-}
-
-order ("speaker")
- */
-
 const inventory = {
   laptop: {
-    count: 32,
-    price: 3999,
+      count: 32,
+      price: 399
   },
   speaker: {
-    count: 65,
-    price: 34.99,
+      count: 65,
+      price: 34.99
   },
   monitor: {
-    count: 12,
-    price: 52.95,
-  },
-};
-function checkInventory(product) {
-  return new Promise((resolve, reject) => {
-    if (inventory[product] > 0) {
-      resolve("Produkt vorhanden");
-    } else {
-      reject("Produkt ausverkauft");
-    }
-  });
+      count: 12,
+      price: 52.95
+  }
 }
 
+function checkInventory(product) {
+  return new Promise((resolve, reject) => {
+      if(inventory[product].count > 0) {
+          resolve("Produkt vorhanden")
+      } else {
+          reject("Produkt ausverkauft")
+      }
+  })
+}
 
 function processPayment(product, balance) {
-    return new Promise((resolve, reject) => {
-        if(balance>=inventory[product].price){
-            resolve("Bezahlen erfolgreich")
-        }else{
-      reject("Nicht genug Guthaben");
-        }
-    });
-  }
-
-
-
+  return new Promise((resolve, reject) => {
+      if(balance >= inventory[product].price) {
+          resolve("Bezahlung erfolgreich")
+      } else {
+          reject("Nicht genug Guthaben")
+      }
+  })
+}
 
 function shipOrder() {
   return new Promise((resolve) => {
-    setTimeout(()=>resolve("Produkt verschickt"),2000);
-  });
+      setTimeout(() => resolve("Produkt verschickt"), 2000)
+  })
 }
 
+// Pyramid of Doom oder Callback Hell
 
-function order(product, balance) {
+/* function order(product, balance) {
   checkInventory(product)
-    .then((productInventory) => {
-      console.log(productInventory);
-      processPayment(product, balance)
-      .then((productPayment)=>{
-        console.log(productPayment)
-        shipOrder()
-        .then((productShipment)=>{
-            console.log(productShipment)
-        })
+      .then((productInventory)=> {
+          console.log(productInventory)
+          processPayment(product, balance)
+              .then((productPayment) => {
+                  console.log(productPayment)
+                  shipOrder()
+                      .then((productShipment) => {
+                          console.log(productShipment)
+                      })
+                      .catch((rejected) => {
+                          console.log(rejected)
+                      })
+              })
+              .catch((rejected) => {
+                  console.log(rejected)
+              })
       })
-    })
-    .catch((rejected) => {
-      console.log(rejected);
-    });
+      .catch((rejected) => {
+          console.log(rejected)
+      })
+} */
+
+
+
+
+
+async function order(product, balance) {
+  try {
+    const productInventory = await checkInventory(product);
+    console.log(productInventory)
+    const productPayment = await processPayment(product, balance);
+    console.log(productPayment)
+    const productShipment = await shipOrder();
+    console.log(productShipment);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 
 
 
 
-
-
-order("speaker", 62.5);
+order("speaker", 62.5)
