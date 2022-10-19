f6f8ddc64c3b7cb1d8b333800cc04b6de098fb87
Huseyin-Gunaydin <gunaydinhsueyin1453@gmail.com>
2022-10-19 09:17:31 +0200

    reacht erstellt

---
+++ react/live/myapp/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const rootDomelement = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+root.render(
+  <App />
+  
+/*   <div>
+    <h1>Hello React!</h1>
+    <p>lorem <strong>ipsum</strong></p>
+  </div> */
+);
+reportWebVitals();
